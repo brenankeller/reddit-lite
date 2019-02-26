96e9211d6e2dfe7bbf7723f2df62e96689701411
Brenan Keller <brenan.keller@gmail.com>
2019-02-25 22:46:04 -0800

    Render post comments.

--- js/reddit.js
+++ js/reddit.js
@@ -1,9 +1,9 @@
-function fetchThreads() {
+function fetchPosts() {
     $.ajax({
         url: 'https://www.reddit.com/r/raspberry_pi/new.json?sort=new',
         type: 'GET',
         success: function(result){
-            renderThreads(result['data']['children'])
+            renderPosts(result['data']['children'])
         },
         error: function(error){
             console.log(`Error ${error}`)
@@ -12,14 +12,57 @@ function fetchThreads() {
     })
 }
 
-function renderThreads(threads) {
-    var threadList = document.getElementById('threadList')
-    threadList.innerHTML = '';
+function renderPosts(threads) {
+    var postList = document.getElementById('postList')
+    postList.innerHTML = '';
 
     const markup = threads.map(thread => `
-        <div class="row">
-            <div class="col-sm-3">${thread['data']['thumbnail_width'] ? `<img src=${thread['data']['thumbnail']}>` : ''}</div>
-            <div class="col-sm-9">${thread['data']['title']}</div>
+        <a href="/comments.html?id=${thread['data']['id']}">
+            <div class="row post">
+                <div class="col-sm-3"><img class="img-fluid" src=${thread['data']['thumbnail_width'] ? `${thread['data']['thumbnail']}` : 'img/reddit-default.jpg'}></div>
+                <div class="col-sm-9">${thread['data']['title']}</div>
+            </div>
+        </a>`)
+    postList.innerHTML = markup.join('')
+}
+
+function fetchComments() {
+    var params = getSearchParameters();
+    $.ajax({
+        url: `https://www.reddit.com/r/raspberry_pi/comments/${params.id}.json`,
+        type: 'GET',
+        success: function(result){
+            renderComments(result[1]['data']['children'])
+        },
+        error: function(error){
+            console.log(`Error ${error}`)
+            return
+        }
+    })
+}
+
+function mapComments(comments) {
+    const markup = comments.map(comment => `
+        <div class="row comment">
+            <div class="col-sm-12">
+                <div class="row comment-author">
+                    <div class="col-sm-6">${comment['data']['author']}</div>
+                    <div class="col-sm-6">${comment['data']['created_utc']}</div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        ${comment['data']['body']}
+                        ${comment['data']['replies'] ? mapComments(comment['data']['replies']['data']['children']) : ''}
+                    </div>
+                </div>
+            </div>
         </div>`)
-    threadList.innerHTML = markup.join('')
+    return markup.join('')
+}
+
+function renderComments(comments) {
+    var commentThread = document.getElementById('commentThread')
+    commentThread.innerHTML = '';
+    var commentHtml = mapComments(comments)
+    commentThread.innerHTML = commentHtml ? commentHtml : 'There are no comments on this post.'
 }
---
+++ js/utils.js
@@ -0,0 +1,16 @@
+// The contents of this file were sourced from the Internet.
+
+function getSearchParameters() {
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+  var params = {};
+  var prmarr = prmstr.split("&");
+  for ( var i = 0; i < prmarr.length; i++) {
+      var tmparr = prmarr[i].split("=");
+      params[tmparr[0]] = tmparr[1];
+  }
+  return params;
+}
